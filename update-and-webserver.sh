992b462e70f96a990025fcf19e437ba6078afdb9
Halfdan <41484873+HalfdanDK@users.noreply.github.com>
2022-01-25 10:04:17 +0100

    Update update-and-webserver.sh

--- update-and-webserver.sh
+++ update-and-webserver.sh
@@ -3,3 +3,6 @@ sudo apt upgrade -y
 sudo apt install nginx
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt install python-certbot-nginx
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw allow 'Nginx Full'
