60ed6dbc73ea0748eb2873934db05de8cd900f60
Halfdan <41484873+HalfdanDK@users.noreply.github.com>
2022-08-23 20:03:43 +0200

    Add files via upload

---
+++ ubuntu-20-04-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+
+# Firewall Stuff
+apt install firewalld -y
+firewall-cmd --add-port=80/tcp --permanent
+firewall-cmd --add-port=80/udp --permanent
+firewall-cmd --add-port=443/udp --permanent
+firewall-cmd --add-port=443/tcp --permanent
+firewall-cmd --add-port=3306/udp --permanent
+firewall-cmd --add-port=3306/tcp --permanent
+firewall-cmd --add-port=22/udp --permanent
+firewall-cmd --add-port=22/tcp --permanent
+firewall-cmd --reload
+systemctl enable --now firewalld
+systemctl start firewalld
+systemctl restart firewalld
+
+# Install Dependencies
+apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git
+sudo apt install -y certbot
+sudo apt install -y python3-certbot-nginx
+systemctl start nginx
+echo Done!
