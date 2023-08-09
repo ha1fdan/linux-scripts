f267ad4943955950249251438c9730ad4c32107b
Halfdan <41484873+HalfdanDK@users.noreply.github.com>
2023-08-09 07:57:33 -0700

    Update php8.1 to php8.2

--- ubuntu-20-04-setup.sh
+++ ubuntu-20-04-setup.sh
@@ -15,7 +15,7 @@ systemctl enable --now firewalld
 systemctl start firewalld
 systemctl restart firewalld
 
-apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git
+apt -y install php8.2 php8.2-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git
 sudo apt install -y certbot
 sudo apt install -y python3-certbot-nginx
 systemctl start nginx
